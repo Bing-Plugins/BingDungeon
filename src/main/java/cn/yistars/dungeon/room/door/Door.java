3f542da2bf02aa70049260238437339200849dee
冰砚炽 <Bing_Yanchi@outlook.com>
2024-09-08 00:52:41 +0800

    基本上连接所有门

--- src/main/java/cn/yistars/dungeon/room/door/Door.java
+++ src/main/java/cn/yistars/dungeon/room/door/Door.java
@@ -4,6 +4,8 @@ import cn.yistars.dungeon.room.Room;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.awt.*;
+
 @Getter
 public class Door {
     private final DoorType type;
@@ -16,5 +18,14 @@ public class Door {
         this.type = type;
         this.x = x;
         this.z = z;
+    }
+
+    public Point getPoint() {
+        return switch (type) {
+            case NORTH -> new Point(x, z + 1);
+            case SOUTH -> new Point(x, z - 1);
+            case EAST -> new Point(x + 1, z);
+            case WEST -> new Point(x - 1, z);
+        };
     }
 }
